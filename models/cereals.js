b6ff4ac1e75f480d1f9a5385f486886d2fdd8a9c
Kenneth-Parker <kennethkavon@gmail.com>
2023-11-06 15:57:00 -0500

    get oneCereal

--- models/cereals.js
+++ models/cereals.js
@@ -1,7 +1,7 @@
 const db = require('../db/dbConfig');
 
 const getAllCereals = async () => {
-    try{
+    try {
         const allCereals = await db.any("SELECT * FROM cereals");
         return allCereals
     } catch (error) {
@@ -9,6 +9,17 @@ const getAllCereals = async () => {
     }
 }
 
+
+const getCereal = async (id) => {
+    try {
+        const oneCereal = await db.one("SELECT * FROM cereals WHERE id=$1", id)
+        return oneCereal
+    } catch (error) {
+        return error
+    }
+}
+
 module.export = {
-    getAllCereals
+    getAllCereals,
+    getCereal
 }
