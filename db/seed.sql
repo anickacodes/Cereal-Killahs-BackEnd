ef94ebf8961ff5f9d6b8edb38524bdba8cd5943f
Kenneth-Parker <kennethkavon@gmail.com>
2023-11-06 14:45:41 -0500

    db set up , env updated to reflect db

---
+++ db/seed.sql
@@ -0,0 +1,13 @@
+\c cereals;
+
+INSERT INTO cereals (name, brand, Type, price, is_favorite) VALUES
+('Honey Nut Cheerios', 'General Mills', 'Whole Grain', 3.99, true),
+('Frosted Flakes', 'Kellogg/s', 'Sugar', 3.49, false),
+('Lucky Charms', 'General Mills', 'Marshmallow', 4.29, true),
+('Cinnamon Toast Crunch', 'General Mills', 'Sugar', 3.79, false),
+('Raisin Bran', 'Post', 'Bran', 3.29, false),
+('Shredded Wheat', 'Kellogg/s', 'Whole Grain', 4.49, true),
+('Cocoa Puffs', 'General Mills', 'Chocolate', 3.99, false),
+('Froot Loops', 'Kellogg/s', 'Sugar', 3.69, false),
+('Trix', 'General Mills', 'Fruit', 4.19, true),
+('Special K', 'Kellogg/s', 'Whole Grain', 3.89, false);
