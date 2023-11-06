ec2588d19665af11e5a9fcfe66716fec96853a8e
anickacodes <anickalewis@pursuit.org>
2023-11-06 16:27:27 -0500

    added assets+, controller Get, Post, Delete

--- controllers/cerealsController.js
+++ controllers/cerealsController.js
@@ -2,9 +2,36 @@ const express = require("express");
 
 const cereals = express.Router();
 
+cereals.get("/", async (req, res) => {
+  const allCereals = await getAllCereals();
+  if (allCereals[0]) {
+    res.status(200).json(allCereals);
+  } else {
+    res.status(500).json({ error: "server error" });
+  }
+});
 
-cereals.get('/', async (req, res) => {
-const allCereals = await getAllCereals()
+cereals.get("/:id", async (req, res) => {
+    const id = req.params.id
+    const oneCereal = await getCereal(id);
+    if (oneCereal) {
+      res.status(200).json(oneCereal);
+    } else {
+      res.status(404).json({ error: "ID Not Found" });
+    }
+});
+
+cereals.post('/', async (req, res) => {
+const postCereal = await createCereal(req.body)
+res.status(200).json(postCereal)
+})
+
+cereals.delete('/:id', async (req, res) => {
+    const id = req.params.id
+    const deletedCereal = await deleteCereal(id)
+    if (deletedCereal.id) {
+        res.status(200).json(deletedCereal)
+    }
 })
 
 module.exports = cereals;
