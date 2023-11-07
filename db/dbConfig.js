deeb194c4109671f9a131678ba9fa5b05852fd86
Kenneth-Parker <kennethkavon@gmail.com>
2023-11-06 22:21:01 -0500

    PG_PASSWORD

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -6,7 +6,8 @@ const cn = {
     host: process.env.PG_HOST,
     port: process.env.PG_PORT,
     database: process.env.PG_DATABASE,
-    user: process.env.PG_USER
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD
 }
 
 const db = pgp(cn)
