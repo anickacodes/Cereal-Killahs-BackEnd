055cf85dbe8ca4a5b5a846e91720a419a0210f99
anickacodes <anickalewis@pursuit.org>
2023-11-08 00:34:51 -0500

    img key , maybe

--- db/seed.sql
+++ db/seed.sql
@@ -2,14 +2,14 @@
 
 \c cereals;
 
-INSERT INTO cereals (name, brand, Type, price, is_favorite, rating) VALUES
-('Honey Nut Cheerios', 'General Mills', 'Whole Grain', 3.99, true, 4),
-('Frosted Flakes', 'Kellogg/s', 'Sugar', 3.49, false, 2),
-('Lucky Charms', 'General Mills', 'Marshmallow', 4.29, true, 4),
-('Cinnamon Toast Crunch', 'General Mills', 'Sugar', 3.79, false, 3),
-('Raisin Bran', 'Post', 'Bran', 3.29, false, 4),
-('Shredded Wheat', 'Kellogg/s', 'Whole Grain', 4.49, true, 3),
-('Cocoa Puffs', 'General Mills', 'Chocolate', 3.99, false, 4),
-('Froot Loops', 'Kellogg/s', 'Sugar', 3.69, false, 2),
-('Trix', 'General Mills', 'Fruit', 4.19, true, 3),
-('Special K', 'Kellogg/s', 'Whole Grain', 3.89, false, 5);
+INSERT INTO cereals (name, brand, Type, price, is_favorite, rating, data) VALUES
+('Honey Nut Cheerios', 'General Mills', 'Whole Grain', 3.99, true, 4, '{"image": "path/to/honey_nut_cheerios_image.jpg"}'::JSONB),
+('Frosted Flakes', 'Kellogg/s', 'Sugar', 3.49, false, 2, '{"image": "path/to/honey_nut_cheerios_image.jpg"}'::JSONB),
+('Lucky Charms', 'General Mills', 'Marshmallow', 4.29, true, 4, '{"image": "path/to/honey_nut_cheerios_image.jpg"}'::JSONB),
+('Cinnamon Toast Crunch', 'General Mills', 'Sugar', 3.79, false, 3, '{"image": "path/to/honey_nut_cheerios_image.jpg"}'::JSONB),
+('Raisin Bran', 'Post', 'Bran', 3.29, false, 4, '{"image": "path/to/honey_nut_cheerios_image.jpg"}'::JSONB),
+('Shredded Wheat', 'Kellogg/s', 'Whole Grain', 4.49, true, 3, '{"image": "path/to/honey_nut_cheerios_image.jpg"}'::JSONB),
+('Cocoa Puffs', 'General Mills', 'Chocolate', 3.99, false, 4, '{"image": "path/to/honey_nut_cheerios_image.jpg"}'::JSONB),
+('Froot Loops', 'Kellogg/s', 'Sugar', 3.69, false, 2, '{"image": "path/to/honey_nut_cheerios_image.jpg"}'::JSONB),
+('Trix', 'General Mills', 'Fruit', 4.19, true, 3, '{"image": "path/to/honey_nut_cheerios_image.jpg"}'::JSONB),
+('Special K', 'Kellogg/s', 'Whole Grain', 3.89, false, 5, '{"image": "path/to/honey_nut_cheerios_image.jpg"}'::JSONB);
