163abea5ab20a7af13e14bf920caf33bbcc64a62
anickacodes <anickalewis@pursuit.org>
2023-11-08 15:03:46 -0500

    reworking imgs

--- db/seed.sql
+++ db/seed.sql
@@ -3,7 +3,7 @@
 \c cereals;
 
 INSERT INTO cereals (name, brand, Type, price, is_favorite, rating, image_url) VALUES
-('Honey Nut Cheerios', 'General Mills', 'Whole Grain', 3.99, true, 4, 'https://banner2.cleanpng.com/20180630/baa/kisspng-breakfast-cereal-honey-nut-cheerios-kashi-golean-c-general-mills-5b374da3d040e6.356870091530351011853.jpg'),
+('Honey Nut Cheerios', 'General Mills', 'Whole Grain', 3.99, true, 4, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmHfqezmU7ymSLZRB0pxV1ezcNlqVpvZS8MMkrJluOrrS-ntjZn1YuR3KT3sveLw7V4co&usqp=CAU'),
 ('Lucky Charms', 'General Mills', 'Marshmallow', 4.29, true, 4, 'https://p7.hiclipart.com/preview/501/751/615/breakfast-cereal-general-mills-lucky-charm-cereal-rice-krispies-treats-lucky-charms-marshmallow-others.jpg'),
 ('Cinnamon Toast Crunch', 'General Mills', 'Sugar', 3.79, false, 3, 'https://www.cinnamontoastcrunch.com/_next/image?url=https%3A%2F%2Fctccmsprod.wpengine.com%2Fwp-content%2Fuploads%2F2022%2F02%2FCTC-Original-New-Packaging-460x460-1.png&w=1080&q=75'),
 ('Raisin Bran', 'Post', 'Bran', 3.29, false, 4, 'https://images.kglobalservices.com/www.raisinbran.com/en_us/product/product_4508651/kicproductimage-127056_kicproductimage-127056.png'),
