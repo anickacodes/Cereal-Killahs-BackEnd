45a7e9a1b2569ceb314559d6ed8eee7930579378
Kenneth-Parker <kennethkavon@gmail.com>
2023-11-09 03:34:23 -0500

    schema seed image_url

--- db/schema.sql
+++ db/schema.sql
@@ -4,11 +4,12 @@ CREATE DATABASE cereals;
 \c cereals;
 
 CREATE TABLE cereals (
-id SERIAL PRIMARY KEY,
-name TEXT NOT NULL,
-brand TEXT NOT NULL,
-Type TEXT,
-price NUMERIC,
-is_favorite BOOLEAN, 
-rating INTEGER CHECK (rating >= 1 AND rating <= 5)
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  brand TEXT NOT NULL,
+  Type TEXT,
+  price NUMERIC,
+  is_favorite BOOLEAN, 
+  rating INTEGER CHECK (rating >= 1 AND rating <= 5),
+  image_url TEXT
 );
