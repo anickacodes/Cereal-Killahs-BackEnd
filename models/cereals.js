2b1df618a2cb01e8f53d8e020af6b727c0e3cf60
Kenneth-Parker <kennethkavon@gmail.com>
2023-11-10 04:21:38 -0500

    added img url schema

--- models/cereals.js
+++ models/cereals.js
@@ -22,8 +22,8 @@ const getCereal = async (id) => {
 const createCereal = async (cereal) => {
     try {
         const newCereal = await db.one(
-            "INSERT INTO cereals (name, brand, type, price, is_favorite, rating) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
-            [cereal.name, cereal.brand, cereal.type, cereal.price, cereal.is_favorite, cereal.rating]
+            "INSERT INTO cereals (name, brand, type, price, is_favorite, rating, image_url) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+            [cereal.name, cereal.brand, cereal.type, cereal.price, cereal.is_favorite, cereal.rating, cereal.image_url]
         )
         return newCereal
     } catch (error) {
